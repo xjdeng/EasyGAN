79d1e7cd1fa22b890432ca65c2d1165532ced2b0
xjdeng <xjd001@gmail.com>
2018-05-21 23:46:09 -0500

    Add train() and also adjust default_discriminator() to take a shape input

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import numpy as np
+from tqdm import tqdm
 from keras.layers import Input
 from keras.models import Model, Sequential
 from keras.layers.core import Reshape, Dense, Dropout, Flatten
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import Conv2D, UpSampling2D
-from keras.datasets import mnist
 from keras.optimizers import Adam
 from keras import backend as K
 from keras import initializers
@@ -25,9 +25,11 @@ def combine(generator, discriminator):
     return gan
     
 
-def default_discriminator():
+def default_discriminator(myshape = (1, 28, 28)):
     discriminator = Sequential()
-    discriminator.add(Conv2D(64, kernel_size=(5, 5), strides=(2, 2), padding='same', input_shape=(1, 28, 28), kernel_initializer=initializers.RandomNormal(stddev=0.02)))
+    discriminator.add(Conv2D(64, kernel_size=(5, 5), strides=(2, 2), \
+                             padding='same', input_shape=myshape, \
+                             kernel_initializer=initializers.RandomNormal(stddev=0.02)))
     discriminator.add(LeakyReLU(0.2))
     discriminator.add(Dropout(0.3))
     discriminator.add(Conv2D(128, kernel_size=(5, 5), strides=(2, 2), padding='same'))
@@ -57,3 +59,50 @@ def default_generator():
 
 def random_image(height, width):
     return np.random.randint(0,255,(height, width,3))
+
+def train(X0, generator = None, discriminator = None, epochs=10, batchSize=128):
+    X = (X0.astype(np.float32) - 127.5)/127.5
+    X = X[:, np.newaxis, :, :]
+    if generator is None:
+        generator = default_generator()
+    if discriminator is None:
+        discriminator = default_discriminator(X.shape[1:])
+    dLosses = []
+    gLosses = []
+    gan = combine(generator, discriminator)
+    batchCount = int(round(X.shape[0] / batchSize))
+    print('Epochs:', epochs)
+    print('Batch size:', batchSize)
+    print('Batches per epoch:', batchCount)
+
+    for e in range(1, epochs+1):
+        print('-'*15, 'Epoch %d' % e, '-'*15)
+        for _ in tqdm(range(batchCount)):
+            # Get a random set of input noise and images
+            noise = np.random.normal(0, 1, size=[batchSize, randomDim])
+            imageBatch = X[np.random.randint(0, X.shape[0], size=batchSize)]
+
+            # Generate fake MNIST images
+            generatedImages = generator.predict(noise)
+            X = np.concatenate([imageBatch, generatedImages])
+
+            # Labels for generated and real data
+            yDis = np.zeros(2*batchSize)
+            # One-sided label smoothing
+            yDis[:batchSize] = 0.9
+
+            # Train discriminator
+            discriminator.trainable = True
+            dloss = discriminator.train_on_batch(X, yDis)
+
+            # Train generator
+            noise = np.random.normal(0, 1, size=[batchSize, randomDim])
+            yGen = np.ones(batchSize)
+            discriminator.trainable = False
+            gloss = gan.train_on_batch(noise, yGen)
+
+        # Store loss of most recent batch from this epoch
+        dLosses.append(dloss)
+        gLosses.append(gloss)
+
+    return (generator, discriminator, dLosses, gLosses)  
